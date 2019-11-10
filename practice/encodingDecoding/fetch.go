3eb13f1b89e88865358a48125dc0a407e910fac3
vanderkilu <vanderkiliku@gmail.com>
2019-11-10 19:22:34 +0000

    update fetch

--- practice/encodingDecoding/fetch.go
+++ practice/encodingDecoding/fetch.go
@@ -5,36 +5,46 @@ import (
 	"log"
 	"fmt"
 	"encoding/json"
+	"io/ioutil"
 )
 
-type Todo struct {
-  userId string `json.userId`
-  id int `json.userId`
-  title string `json.title`
-  completed string `json.completed`
-}
-
-type TodoList struct {
-	results []Todo `json.results`
-}
+type (
+	Todo struct {
+		UserId int `json:"userId"`
+		Id int `json:"id"`
+		Title string `json:"title"`
+		Completed bool `json:"completed"`
+	}
+)
 
 func main() {
 	const url = "https://jsonplaceholder.typicode.com/todos/"
 
 	response, err := http.Get(url)
-	defer response.Body.Close()
-
 	if err != nil {
 		log.Fatalln("something went wrong fetching the url")
 	}
-	fmt.Println(response)
 
-	var todoListResp TodoList
-	err = json.NewDecoder(response.Body).Decode(&todoListResp)
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln("something went wrong fetching the url")
+	}
+
+	defer response.Body.Close()
+
+	var todoListResp []Todo
+	err = json.Unmarshal(body, &todoListResp)
 
 	if err != nil {
-		log.Fatalln("couldn't decode into todoListResp")
+		log.Fatalln(err)
 	}
-	fmt.Println(todoListResp)
+	
+	for _, item := range todoListResp {
+		fmt.Printf("{\n")
+		fmt.Printf("\tid: %d\n", item.Id)
+		fmt.Printf("\ttitle: %s\n", item.Title)
+		fmt.Printf("\tcompleted: %v\n", item.Completed)
+		fmt.Printf("}\n")
+	}
 
 }
