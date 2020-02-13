e9f366c39c73b27487abbda5eebf006f0966ddd2
vanderkilu <vanderkiliku@gmail.com>
2020-02-13 01:01:44 +0000

    add sitemap functionality

--- exercises/siteMap/main.go
+++ exercises/siteMap/main.go
@@ -2,16 +2,77 @@ package main
 
 import (
 	"flag"
+	"io"
+	"net/http"
+	"net/url"
+	"fmt"
 	"strings"
+	"github.com/vanderkilu/goexercises/exercises/link"
 )
 
-type Link struct {
-	Href string
-	Text string
+func extractLinks(r io.Reader, base string) []string {
+	var hrefs []string
+	links := link.LinkParser(r)
+	for _, link := range links {
+		if strings.HasPrefix(link.Href,"/") {
+			hrefs = append(hrefs, base + link.Href)
+		}
+		if strings.HasPrefix(link.Href, "http") {
+			hrefs = append(hrefs, link.Href)
+		}
+		
+	}
+	return hrefs
 }
 
-func parseLinks(baseUrl string) []Link {
-	links := LinkParser(strings.NewReader(baseUrl))
+func filterLinks(links []string, base string) []string {
+	var filteredLinks []string
+	for _, link := range links {
+		if strings.HasPrefix(link, base) {
+			filteredLinks = append(filteredLinks, link)
+		}
+	}
+	return filteredLinks
+}
+
+func get(inputUrl string) []string {
+	resp, err := http.Get(inputUrl)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	base := &url.URL{
+		Host: resp.Request.URL.Host,
+		Scheme: resp.Request.URL.Scheme,
+	}
+	links := extractLinks(resp.Body, base.String())
+	links = filterLinks(links, base.String())
+	return links
+}
+
+func getAllLinks(url string, maxDepth int) []string {
+	cache := make(map[string]struct{})
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		url: struct{}{},
+	}
+	for i := 0; i < maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		for k,_ := range q {
+			if _, ok := cache[k]; ok {
+				continue
+			}
+			cache[k] = struct{}{}
+			for _, link := range get(url) {
+				nq[link] = struct{}{}
+			}
+		}
+	}
+	var links []string
+	for k, _ := range cache {
+		links = append(links,k)
+	}
 	return links
 }
 
@@ -21,7 +82,14 @@ func main() {
 	//2. Go through each link and get all associated links recursively
 	//3. Build a sitemap by encoding the result in an xml
 
-	baseUrl := flag.String("url", "http://gophercises.com", "the url you want to parse")
+	inputUrl := flag.String("url", "http://gophercises.com", "the url you want to parse")
 	flag.Parse()
-	parseLinks(*baseUrl)
+
+	links := getAllLinks(*inputUrl,5)
+
+	for _, link := range links {
+		fmt.Println(link)
+	}
+
+	
 }
